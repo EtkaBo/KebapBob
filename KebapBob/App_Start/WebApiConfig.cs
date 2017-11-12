db77c0ca3af472e01f1434096da58ae97abcbf63
etkabayramoglu <etkabo@gmail.com>
2017-11-12 16:55:58 -0600

    Initial Commit

--- KebapBob/App_Start/WebApiConfig.cs
+++ KebapBob/App_Start/WebApiConfig.cs
@@ -9,13 +9,18 @@ namespace KebapBob
 {
     public static class WebApiConfig
     {
+
+
+        public static string UrlPrefix { get { return "api"; } }
+        public static string UrlPrefixRelative { get { return "~/api"; } }
+
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: WebApiConfig.UrlPrefix + "/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
--- KebapBob/Global.asax.cs
+++ KebapBob/Global.asax.cs
@@ -31,7 +31,20 @@ namespace KebapBob
 
 
         }
-        
+
+        protected void Application_PostAuthorizeRequest()
+        {
+            if (IsWebApiRequest())
+            {
+                HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
+            }
+        }
+
+        private bool IsWebApiRequest()
+        {
+            return HttpContext.Current.Request.AppRelativeCurrentExecutionFilePath.StartsWith(WebApiConfig.UrlPrefixRelative);
+        }
+
     }
 
 
